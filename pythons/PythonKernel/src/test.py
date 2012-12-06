7447361bcda0a4df72fc9909f563959ca351abc9
Sage Lui <superalsrk@gmail.com>
2012-12-06 15:36:35 +0800

    forth v3

--- pythons/PythonKernel/src/test.py
+++ pythons/PythonKernel/src/test.py
@@ -1,11 +1,9 @@
-import sys  
-from PyQt4 import QtGui,QtWebKit  
-   
-app = QtGui.QApplication(sys.argv)  
-view = QtWebKit.QWebView()  
-frame = view.page().mainFrame()  
-frame.setHtml("hello world")  
-str = frame.toPlainText()  
-print str.toUtf8()  
-view.show()  
-app.exec_() 
+
+def simple_app(environ, start_response):
+    status = '200 OK'
+    response_headers = [('Content-type','text/plain')]
+    start_response(status, response_headers)
+    return ['hello world!\n']
+
+class AppClass:
+    
---
+++ wsgi.md
@@ -0,0 +1,4 @@
++ 网关：就是在协议之间进行转换
++ wsgi app,wsg container,wsg_middleware
+
+
